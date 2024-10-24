1cabbb585449f679af535751153dfa4e237ff75d
Damien <yithanglee@gmail.com>
2024-10-24 11:18:01 +0800

    upate lint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -65,7 +65,7 @@ export default function LoginPage() {
         </CardContent>
         <CardFooter>
           <p className="text-sm text-center w-full text-gray-600">
-            Don't have an account?{" "}
+            Dont have an account?{" "}
             <Link href="/signup" className="text-blue-600 hover:underline">
               Sign up
             </Link>
