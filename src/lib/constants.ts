c8fa42b6866acd4f3bddba8c0e40fefc7af5aebb
Damien <yithanglee@gmail.com>
2024-11-01 11:34:04 +0800

    housekeeping: update wss

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,8 @@
 // export const PHX_HTTP_PROTOCOL = process.env.PHX_HTTP_PROTOCOL || 'http://';  
 // export const PHX_ENDPOINT = process.env.PHX_ENDPOINT || 'localhost:4000';  
 export const PHX_HTTP_PROTOCOL = process.env.PHX_HTTP_PROTOCOL || 'https://';  // Default to 'http' if not set
+export const PHX_WS_PROTOCOL = process.env.PHX_WS_PROTOCOL || 'wss://';  // Default to 'http' if not set
+
 export const PHX_ENDPOINT = process.env.PHX_ENDPOINT || 'v4.jimatlabs.com';  // Default to localhost if not set
 
 // Cookie Names
